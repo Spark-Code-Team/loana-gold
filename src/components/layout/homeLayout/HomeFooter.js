d82984cc78cd217c70bfc6f54e084dd7d0289ad5
fatemesabbagh <sabbagh.fateme78@gmail.com>
2025-01-21 10:35:44 -0500

    the footer is under construction

--- src/components/layout/homeLayout/HomeFooter.js
+++ src/components/layout/homeLayout/HomeFooter.js
@@ -1,8 +1,69 @@
 
 const HomeFooter = () =>{
     return(
-        <>
-        </>
+        
+
+        <footer className="w-full h-[512px] mt-12 bg-slate-300 py-4">
+
+            <div className="container  mx-auto h-[480px] my-auto  ">
+
+            <div className="h-[104px]">
+            <h3 className="text-lg font-semibold mb-2">درباره داریک</h3> 
+            </div>
+
+            <div className="flex flex-row
+             h-[206px] justify-between my-4">
+
+            <div className="w-[392px]">
+            
+            <p className="text-sm">
+                
+            لونا اولین پلتفرم معاملات طلا در ایران است که به
+             کاربران امکان می‌دهد تا به صورت مستقیم و به شیوۀ "Customer to Customer (C2C)" 
+            به معامله طلا پرداخته و از پروژه ایجاد یک پلتفرم
+             امن، شفاف و سریع برای معامله طلا میان کاربران بهره‌مند شوند.
+
+
+            </p> 
+            
+            </div>
+
+
+            <div className="">
+            <h3 className="text-lg text-[#AB8B53] font-semibold mb-2 w-[208px]">اطلاعات تماس</h3>
+            <p className="text-sm">آدرس دفتر مرکزی: سعادت آباد، خیابان کاج، پلاک 16</p> 
+            <p className="text-sm py-6">تلفن: 021-12345678</p> 
+            <p className="text-sm">ایمیل: Drik@gmail.com</p>
+            </div>
+           
+            <div className="">
+            <h3 className="text-lg text-[#AB8B53] font-semibold mb-2">دسترسی سریع</h3>
+            <ul className="space-y-2"> <li><a href="#" className="text-black hover:underline">صفحه اصلی</a></li>
+            <li><a href="#" className="text-black hover:underline">محصولات</a></li> 
+            <li><a href="#" className="text-black hover:underline">سؤالات متداول</a></li> 
+            <li><a href="#" className="text-black hover:underline">تماس با ما</a></li> </ul>
+            </div> 
+
+
+            <div className="">
+            <h3 className="text-lg text-[#AB8B53] font-semibold mb-2"> درباره داریک </h3>
+            <ul className="space-y-2"> <li><a href="#" className="text-black hover:underline">صفحه اصلی</a></li>
+            <li><a href="#" className="text-black hover:underline">محصولات</a></li> 
+            <li><a href="#" className="text-black hover:underline">سؤالات متداول</a></li> 
+            </ul>
+            </div>
+  
+            </div>
+
+            <div className="h-[104px]">
+            <h3 className="text-lg font-semibold mb-2">درباره داریک</h3> 
+            </div>  
+
+            </div>
+
+           
+
+        </footer>
     )
 }
 export default HomeFooter;
