7e2623ffa574ae832d5eb74df5b14028f622167f
fatemesabbagh <sabbagh.fateme78@gmail.com>
2025-03-11 09:22:54 -0400

    unsettled has been done

--- src/components/modules/installments/Settled.js
+++ src/components/modules/installments/Settled.js
@@ -4,7 +4,7 @@ const Settled = () => {
     return(
         <div className="
         w-[912px]
-        h-[585px]
+        h-[900px]
         border-[1px]
         border-[#CBCED7]
         rounded-2xl
@@ -24,7 +24,7 @@ const Settled = () => {
                 <button className="
                 w-[101]
                 h-8
-                bg-[#D1DDF1]
+                bg-[#F6F6F6]
                 rounded-xl
                 mx-8
                 text-sm
@@ -36,7 +36,7 @@ const Settled = () => {
                 <button className="
                 w-[101]
                 h-8
-                bg-[#F6F6F6]
+                bg-[#D1DDF1]
                 rounded-xl
                 mx-8
                 text-sm
@@ -45,23 +45,11 @@ const Settled = () => {
                 >تسویه شده
                 </button>
 
-                <button className="
-                w-[101]
-                h-8
-                bg-[#F6F6F6]
-                rounded-xl
-                mx-8
-                text-sm
-                font-bold
-                "
-                > پیش از موعد
-                </button>
-
             </div>
 
             <div className="
             w-[848px]
-            h-[385px]
+            h-[330px]
             rounded-lg
             border-[1px]
             border-[#EDEDED]
@@ -75,18 +63,12 @@ const Settled = () => {
                     <div className="flex flex-col gap-y-3">
 
                     <p>
-                    بدهی قابل پرداخت:
-                    <span className="text-[#E90000] px-1">
-                      ( جریمه دیر کرد) 665 +
-                    </span>
+                    بدهی قابل پرداخت: 
                     ۱۰۰,۰۰۰,۰۰۰ تومان
                     </p>
 
                     <p>
-                    تاریخ سر رسید: 
-                    <span className="text-[#E90000] px-1">
-                    2 روز از مهلت پرداخت گذشته
-                    </span>
+                    تاریخ سر رسید: پایان روز 1403/04/10
                     </p>
 
                     <p>
@@ -107,25 +89,10 @@ const Settled = () => {
 
                     <p>
                     وضعیت:
-                    <span className="text-[#E90000] px-1">
-                    تسویه نشده
+                    <span className="text-[#096D39] px-1">
+                    تسویه شده
                     </span>
                     </p>
-
-                    </div>
-
-                    <div className="
-                    w-[616px]
-                    h-10
-                    bg-[#FFE0E0]
-                    mt-4
-                    flex 
-                    items-center
-                    justify-center
-                    ">
-                        <p className="text-[#E90000]">
-                        توجه: در صورت عدم پرداخت تا یک هفته پس از مهلت پرداخت خرید شما لغو میشود. 
-                        </p>
 
                     </div>
 
@@ -141,6 +108,7 @@ const Settled = () => {
                          bg-primary
                          rounded-lg
                          ml-5
+                         text-white
                         ">
                             پرداخت
                         </button>
@@ -149,6 +117,79 @@ const Settled = () => {
                 </div>
              
             </div>
+
+            <div className="
+            w-[848px]
+            h-[330px]
+            rounded-lg
+            border-[1px]
+            border-[#EDEDED]
+            mx-8
+            mt-8
+            ">
+                <div className="
+                pr-4
+                pt-4
+                ">
+                    <div className="flex flex-col gap-y-3">
+
+                    <p>
+                    بدهی قابل پرداخت: 
+                    ۱۰۰,۰۰۰,۰۰۰ تومان
+                    </p>
+
+                    <p>
+                    تاریخ سر رسید: پایان روز 1403/04/10
+                    </p>
+
+                    <p>
+                    دوره: 7
+                    </p>
+
+                    <p>
+                    از تاریخ: 1403/04/10
+                    </p>
+
+                    <p>
+                    تا تاریخ: 1404/10/7
+                    </p>
+
+                    <p>
+                    کل بدهی: 180,761,980
+                    </p>
+
+                    <p>
+                    وضعیت:
+                    <span className="text-[#096D39] px-1">
+                    تسویه شده
+                    </span>
+                    </p>
+
+                    </div>
+
+                    <div className="
+                     mt-4
+                     flex
+                     justify-end
+
+                    ">
+                        <button className="
+                         w-20
+                         h-10
+                         bg-primary
+                         rounded-lg
+                         ml-5
+                         text-white
+                        ">
+                            پرداخت
+                        </button>
+                    </div>
+                    
+                </div>
+             
+            </div>
+
+            
 
         </div>
     )
--- src/components/templates/InstallmentsDashboardPage.js
+++ src/components/templates/InstallmentsDashboardPage.js
@@ -1,9 +1,13 @@
 import Settled from "../modules/installments/Settled";
+import Unsettled from "../modules/installments/Unsettled";
 
 const InstallmentDashboardPage = () => {
     return(
         <>
         <Settled/>
+
+        {/* <Unsettled/> */}
+        
         
         </>
     )
