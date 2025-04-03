93febc8abf7ba3831f3eac409435d619c903b02d
fatemesabbagh <sabbagh.fateme78@gmail.com>
2025-04-03 15:28:18 -0400

    ticket

---
+++ src/app/admin/tickets/page.js
@@ -0,0 +1,8 @@
+import AdminTicketPage from "@/components/templates/AdminTicketsPage";
+
+const Tickets = () =>{
+    return(
+      <AdminTicketPage/>
+    )
+}
+export default Tickets;
---
+++ src/components/templates/AdminTicketsPage.js
@@ -0,0 +1,176 @@
+const AdminTicketPage = () =>{
+    return (
+        <div className="
+        w-[1016px] 
+        h-[1000]
+        rounded-xl
+        ">
+            <div className="
+            w-[843px]
+            h-[56px]
+            flex
+            flex-row
+            justify-around
+            border-[1px]
+            rounded-xl
+            items-center
+            
+            ">
+                <div className="">فیلترها</div>
+
+                <div className="
+                border-[1px] 
+                h-[56px] 
+                w-[203px] 
+                justify-center 
+                flex 
+                items-center
+                ">مرتب سازی بر اساس</div>
+
+                <div>موضوع تیکت</div>
+
+                <div className="
+                border-[1px] 
+                h-[56px] 
+                w-[155px] 
+                flex 
+                justify-center 
+                items-center
+                ">تاریخ دریافت</div>
+
+                <div className="text-[#E90000]">حدف همه فیلترها</div>
+
+            </div>
+
+            <div className="
+            w-[1016px]
+            h-[860px]
+            border-[1px]
+            rounded-xl
+            mt-6
+            ">
+                <div className="
+                w-[1009px]
+                h-[148px]
+                border-b-[1px]
+                space-y-3
+                pr-4
+                mt-6
+                ">
+                    <p>
+                    مشکلات فنی
+                    </p>
+                    <p>
+                    1403/12/1
+                    </p>
+                    <p>
+                    سلام، می‌خواهم به شما اطلاع دهم که
+                     در حین استفاده از سرویس شما، با چند مسئله فنی مواجه
+                     شده‌ام که برای من مشکلاتی ایجاد کرده است.   
+                    </p>
+                    <p className="text-[#2FA766]">
+                    #پاسخ داده شده
+                    </p>
+                </div>
+
+                <div className="
+                w-[1009px]
+                h-[148px]
+                border-b-[1px]
+                space-y-3
+                pr-4
+                mt-6
+                ">
+                    <p>
+                    مشکلات فنی
+                    </p>
+                    <p>
+                    1403/12/1
+                    </p>
+                    <p>
+                    سلام، می‌خواهم به شما اطلاع دهم که
+                     در حین استفاده از سرویس شما، با چند مسئله فنی مواجه
+                     شده‌ام که برای من مشکلاتی ایجاد کرده است.   
+                    </p>
+                    <p className="text-[#2FA766]">
+                    #پاسخ داده شده
+                    </p>
+                </div>
+
+                <div className="
+                w-[1009px]
+                h-[148px]
+                border-b-[1px]
+                space-y-3
+                pr-4
+                mt-6
+                ">
+                    <p>
+                    مشکلات فنی
+                    </p>
+                    <p>
+                    1403/12/1
+                    </p>
+                    <p>
+                    سلام، می‌خواهم به شما اطلاع دهم که
+                     در حین استفاده از سرویس شما، با چند مسئله فنی مواجه
+                     شده‌ام که برای من مشکلاتی ایجاد کرده است.   
+                    </p>
+                    <p className="text-[#ECB000]">
+                    #در انتظار پاسخ
+                    </p>
+                </div>
+
+                <div className="
+                w-[1009px]
+                h-[148px]
+                border-b-[1px]
+                space-y-3
+                pr-4
+                mt-6
+                ">
+                    <p>
+                    مشکلات فنی
+                    </p>
+                    <p>
+                    1403/12/1
+                    </p>
+                    <p>
+                    سلام، می‌خواهم به شما اطلاع دهم که
+                     در حین استفاده از سرویس شما، با چند مسئله فنی مواجه
+                     شده‌ام که برای من مشکلاتی ایجاد کرده است.   
+                    </p>
+                    <p className="text-[#2FA766]">
+                    #پاسخ داده شده
+                    </p>
+                </div>
+
+                <div className="
+                w-[1009px]
+                h-[148px]
+                space-y-3
+                pr-4
+                mt-6
+                ">
+                    <p>
+                    مشکلات فنی
+                    </p>
+                    <p>
+                    1403/12/1
+                    </p>
+                    <p>
+                    سلام، می‌خواهم به شما اطلاع دهم که
+                     در حین استفاده از سرویس شما، با چند مسئله فنی مواجه
+                     شده‌ام که برای من مشکلاتی ایجاد کرده است.   
+                    </p>
+                    <p className="text-[#2FA766]">
+                    #پاسخ داده شده
+                    </p>
+                </div>
+
+            </div>
+        </div>
+        
+    )
+}
+export default AdminTicketPage;
